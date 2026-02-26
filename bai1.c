c53d51be20f1c0f1f060232169ecec71324f30be
Nghianvps <nghianguyen02062002@gmail.com>
2026-02-26 08:54:13 +0700

    Bai 1

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [NGUYEN VAN NGHIA]
+ * MSSV:      [PS43782]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH ĐẾM NGUYÊN ÂM VÀ PHỤ ÂM CỦA 1 CHUỖI 
@@ -12,3 +12,35 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+
+int main()
+{
+    char s[100];
+    printf("Nhap vao mot chuoi: ");
+    gets(s);
+
+    int i = 0;
+    int nguyenAm = 0;
+    int phuAm = 0;
+
+    while (s[i] != '\0')
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y' ||
+                s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y')
+            {
+                nguyenAm++;
+            }
+            else
+            {
+                phuAm++;
+            }
+        }
+        i++;
+    }
+
+    printf("Chuoi '%s' co %d nguyen am va %d phu am\n", s, nguyenAm, phuAm);
+    return 0;
+}
